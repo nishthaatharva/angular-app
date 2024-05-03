e0cef17a0ee4cfc78f933dafa598eada3ca2b33e
nishatha <nishtha@atharvasystem.com>
2024-05-03 18:14:39 +0530

    component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabPanelComponent } from './components/tab-panel/tab-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
