a069d7f502d94b1c42a3a27bd9eaf4522b7ad5f5
nishatha <nishtha@atharvasystem.com>
2024-05-03 18:02:28 +0530

    initial

---
+++ src/app/components/tab-panel/tab-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-panel.component.html',
+  styleUrl: './tab-panel.component.css'
+})
+export class TabPanelComponent {
+
+}
