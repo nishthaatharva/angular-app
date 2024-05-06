63183403c90fc55b0b0c1a0dbde7ef1d087d867e
nishatha <nishtha@atharvasystem.com>
2024-05-06 12:28:34 +0530

    Tabs changes

--- src/app/components/tab-panel/tab-panel.component.ts
+++ src/app/components/tab-panel/tab-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
 @Component({
   selector: 'app-tab-panel',
   standalone: true,
@@ -14,21 +13,21 @@ export class TabPanelComponent implements OnInit {
   prevY = 0;
   ctx: CanvasRenderingContext2D | null = null;
   currentColor = 'black';
-
+  activeTab: string = 'Canvas'; // Set the initial active tab to 'Canvas'
   ngOnInit() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
   }
-
+  selectTab(tab: string) {
+    this.activeTab = tab;
+  }
   mousedown(event: MouseEvent) {
     this.isDrawing = true;
     this.prevX = event.clientX - this.canvas.nativeElement.offsetLeft;
     this.prevY = event.clientY - this.canvas.nativeElement.offsetTop;
   }
-
   mouseup() {
     this.isDrawing = false;
   }
-
   mousemove(event: MouseEvent) {
     if (this.isDrawing) {
       const currentX = event.clientX - this.canvas.nativeElement.offsetLeft;
@@ -38,7 +37,6 @@ export class TabPanelComponent implements OnInit {
       this.prevY = currentY;
     }
   }
-
   draw(startX: number, startY: number, endX: number, endY: number) {
     if (this.ctx) {
       this.ctx.beginPath();
@@ -49,7 +47,6 @@ export class TabPanelComponent implements OnInit {
       this.ctx.stroke();
     }
   }
-
   clearCanvas() {
     if (this.ctx) {
       this.ctx.clearRect(
@@ -61,7 +58,6 @@ export class TabPanelComponent implements OnInit {
       this.currentColor = 'black';
     }
   }
-
   setColor(color: string) {
     this.currentColor = color;
   }
