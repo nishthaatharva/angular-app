4ccb35e2433cd750a6cf089b24142a585d61359c
nishatha <nishtha@atharvasystem.com>
2024-05-06 11:58:12 +0530

    styling changes

--- src/app/components/tab-panel/tab-panel.component.ts
+++ src/app/components/tab-panel/tab-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-tab-panel',
   standalone: true,
   templateUrl: './tab-panel.component.html',
-  styleUrls: ['./tab-panel.component.css']
+  styleUrls: ['./tab-panel.component.css'],
 })
 export class TabPanelComponent implements OnInit {
   @ViewChild('canvas', { static: true })
@@ -13,17 +14,21 @@ export class TabPanelComponent implements OnInit {
   prevY = 0;
   ctx: CanvasRenderingContext2D | null = null;
   currentColor = 'black';
+
   ngOnInit() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
   }
+
   mousedown(event: MouseEvent) {
     this.isDrawing = true;
     this.prevX = event.clientX - this.canvas.nativeElement.offsetLeft;
     this.prevY = event.clientY - this.canvas.nativeElement.offsetTop;
   }
+
   mouseup() {
     this.isDrawing = false;
   }
+
   mousemove(event: MouseEvent) {
     if (this.isDrawing) {
       const currentX = event.clientX - this.canvas.nativeElement.offsetLeft;
@@ -33,21 +38,30 @@ export class TabPanelComponent implements OnInit {
       this.prevY = currentY;
     }
   }
+
   draw(startX: number, startY: number, endX: number, endY: number) {
     if (this.ctx) {
       this.ctx.beginPath();
       this.ctx.moveTo(startX, startY);
       this.ctx.lineTo(endX, endY);
-      this.ctx.strokeStyle = this.currentColor; // Use current color
+      this.ctx.strokeStyle = this.currentColor;
       this.ctx.lineWidth = 2;
       this.ctx.stroke();
     }
   }
+
   clearCanvas() {
     if (this.ctx) {
-      this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      this.ctx.clearRect(
+        0,
+        0,
+        this.canvas.nativeElement.width,
+        this.canvas.nativeElement.height
+      );
+      this.currentColor = 'black';
     }
   }
+
   setColor(color: string) {
     this.currentColor = color;
   }
